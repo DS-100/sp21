2a78894536eb4c07bca4f16d459fff84e0f030bc
Chris Pyles <cpyles@berkeley.edu>
2021-02-12 14:36:05 -0800

    hw4

---
+++ hw/hw4/tests/q4biii.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q4biii',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> re.findall(hashtag_re, " #thisisahashtag")\n[\'thisisahashtag\']', 'hidden': False, 'locked': False},
+                                   {'code': '>>> re.findall(hashtag_re, "#thisisahashtag")\n[]', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
