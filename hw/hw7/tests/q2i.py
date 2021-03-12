d4dabcf1f22badea415cfb4a3e2852c308745653
Paul Shao <paulshaoyuqiao1@berkeley.edu>
2021-03-12 15:39:57 -0800

    added hw7 student version

---
+++ hw/hw7/tests/q2i.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2i',
+    'points': 3,
+    'suites': [   {   'cases': [{'code': '>>> set([q2statement1, q2statement2, q2statement3]).issubset({False, True})\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw7/tests/q5i.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q5i',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> isinstance(num_neighborhoods, int)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
